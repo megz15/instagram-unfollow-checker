212acf36ad43ef03ce51193f9c3745a8e1cbe408
megz15 <meghraj.g16@gmail.com>
2023-12-08 18:48:31 +0530

    init: initial commit

---
+++ a.py
@@ -0,0 +1,16 @@
+from requests import get
+from json import dump, load
+
+with open('ids.json') as f:
+    ids = load(f)
+    user_id = ids["user_id"]
+    session_id = ids["session_id"]
+
+data = get(
+    "https://i.instagram.com/api/v1/friendships/{}/followers".format(user_id), cookies= {
+        'ds_user_id': user_id,
+        'sessionid': session_id
+    }).json()
+
+with open('followers.json', 'w') as f:
+    dump(data["users"], f)
