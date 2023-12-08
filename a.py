2069fb5318b814e64a235e030ee895252c85834c
megz15 <meghraj.g16@gmail.com>
2023-12-08 18:52:00 +0530

    fix: UA

--- a.py
+++ a.py
@@ -7,10 +7,14 @@ with open('ids.json') as f:
     session_id = ids["session_id"]
 
 data = get(
-    "https://i.instagram.com/api/v1/friendships/{}/followers".format(user_id), cookies= {
+    "https://i.instagram.com/api/v1/friendships/{}/followers".format(user_id), headers= {
+        'user-agent':'Instagram 420.0.0.0.69'
+    }, cookies= {
         'ds_user_id': user_id,
         'sessionid': session_id
     }).json()
 
-with open('followers.json', 'w') as f:
-    dump(data["users"], f)
+# with open('followers.json', 'w') as f:
+#     dump(data["users"], f)
+
+print(data["users"])
