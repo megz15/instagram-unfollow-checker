28d697fd8d604886db87cd8980615a1368e6cf5c
megz15 <meghraj.g16@gmail.com>
2023-12-08 19:01:54 +0530

    feat: get all followers

--- a.py
+++ a.py
@@ -6,27 +6,34 @@ with open('ids.json') as f:
     user_id = ids["user_id"]
     session_id = ids["session_id"]
 
+max_id = ''
 users = {}
 
-data = get(
-    "https://i.instagram.com/api/v1/friendships/{}/followers".format(user_id), headers= {
-        'user-agent':'Instagram 420.0.0.0.69'
-    }, cookies= {
-        'ds_user_id': user_id,
-        'sessionid': session_id
-    }).json()
+while True:
+    data = get(
+        "https://i.instagram.com/api/v1/friendships/{}/followers?max_id={}".format(user_id, max_id), headers= {
+            'user-agent':'Instagram 420.0.0.0.69'
+        }, cookies= {
+            'ds_user_id': user_id,
+            'sessionid': session_id
+        }).json()
 
-for i in data["users"]:
-    if i["pk"] not in users:
-        users[len(users)] = {
-            "id": i["pk"],
-            "username": i["username"],
-            "name": i["full_name"],
-            "pfp": i["profile_pic_url"],
-            "is_private": i["is_private"]
-        }
+    try:
+        max_id = data["next_max_id"]
+    except KeyError:
+        break
 
-# with open('followers.json', 'w') as f:
-#     dump(data["users"], f)
+    for i in data["users"]:
+        if i["pk"] not in users:
+            users[len(users)] = {
+                "id": i["pk"],
+                "username": i["username"],
+                "name": i["full_name"],
+                "pfp": i["profile_pic_url"],
+                "is_private": i["is_private"]
+            }
+
+with open('followers.json', 'w') as f:
+    dump(data["users"], f)
 
 print(users)
