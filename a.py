a0af3d0b36c8e98f7729d0d2148a8ad77eb0ed72
megz15 <meghraj.g16@gmail.com>
2023-12-08 18:54:01 +0530

    feat: unique users

--- a.py
+++ a.py
@@ -6,6 +6,8 @@ with open('ids.json') as f:
     user_id = ids["user_id"]
     session_id = ids["session_id"]
 
+users = {}
+
 data = get(
     "https://i.instagram.com/api/v1/friendships/{}/followers".format(user_id), headers= {
         'user-agent':'Instagram 420.0.0.0.69'
@@ -14,7 +16,17 @@ data = get(
         'sessionid': session_id
     }).json()
 
+for i in data["users"]:
+    if i["pk"] not in users:
+        users[len(users)] = {
+            "id": i["pk"],
+            "username": i["username"],
+            "name": i["full_name"],
+            "pfp": i["profile_pic_url"],
+            "is_private": i["is_private"]
+        }
+
 # with open('followers.json', 'w') as f:
 #     dump(data["users"], f)
 
-print(data["users"])
+print(users)
